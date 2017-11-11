187892e5bfd9498b53fc2eaf1f0c07527d3555ab
tom-cat <letom90@gmail.com>
2017-11-11 17:28:34 +0530

    Update NumberWordsTests.java

--- Numbers/test/devops/numbers/NumberWordsTests.java
+++ Numbers/test/devops/numbers/NumberWordsTests.java
@@ -5,47 +5,29 @@ import org.junit.*;
 import devops.numbers.NumberWords;
 
 public class NumberWordsTests {
-	private NumberWords numberWords ;
-	
+	private NumberWords numberWords;
+
 	@Before
 	public void setup() {
-		numberWords = new NumberWords() ;
+		numberWords = new NumberWords();
 	}
 
-	
-   
 	@Test
-	public void numberOneReturnsOne() {
-		Assert.assertEquals( "One", "1", numberWords.toWords( 1 ) ) ;
+	public void numberOutOfRangeReturnsError() {
+		Assert.assertEquals("Error", "Number out of range",
+				numberWords.toWords(-1));
 	}
-	
+
 	@Test
-	public void numberTwoReturnsTwo() {
-		Assert.assertEquals( "Two", "2", numberWords.toWords( 2 ) ) ;
+	public void convertDigitsToWords() {
+		Assert.assertEquals("Three", numberWords.toWords(3));
+		Assert.assertEquals("Twenty Five", numberWords.toWords(25));
+		Assert.assertEquals("One Hundred", numberWords.toWords(100));
+		Assert.assertEquals("Two Hundred Ninety Five", numberWords.toWords(295));
+		Assert.assertEquals("Five Hundred", numberWords.toWords(500));
+		Assert.assertEquals("Nine Hundred Ninety Nine",
+				numberWords.toWords(999));
+		Assert.assertEquals("One Thousand One", numberWords.toWords(1001));
 	}
-	
-	@Test
-	public void numberThreeReturnsThree() {
-		Assert.assertEquals( "Three", "3", numberWords.toWords( 3 ) ) ;
-	}
-	@Test
-	public void numberFourReturnsFour() {
-		Assert.assertEquals( "Four", "4", numberWords.toWords( 4 ) ) ;
-	}
-	
-	@Test
-	public void numberFiveReturnsFive() {
-		Assert.assertEquals( "Five", "5", numberWords.toWords( 5 ) ) ;
-	}
-	
-	@Test
-	public void numberSixReturnsSix() {
-		Assert.assertEquals( "Six", "6", numberWords.toWords( 6 ) ) ;
-	}
-	
-	@Test
-	public void numberSevenReturnsSeven() {
-		Assert.assertEquals( "Seven", "7", numberWords.toWords( 7 ) ) ;
-	}
-	
+
 }
